93c1ca4025ef5478fb5a4c5f6aaaa219f7da30e6
shenhaiyu <543209619@qq.com>
2016-09-07 20:31:18 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/handler26/MainActivity.java
@@ -0,0 +1,73 @@
+package cn.edu.bistu.cs.se.handler26;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView = (TextView) findViewById(R.id.txtContent);
+//使用Handler的post()方法将Runnable对象从后台线程发送给主线程
+        Handler handler1 = new Handler();
+        handler1.post(new Runnable(){
+            @Override
+            public void run() {
+                textView.setText("123");
+            }
+        });
+
+        final Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                textView.setText(msg.arg1+"");
+            }
+        };
+
+
+        final Runnable myWorker = new Runnable() {
+            @Override
+            public void run() {
+                int progress = 0;
+                while(progress <= 100){
+                    Message msg = new Message();
+                    msg.arg1 = progress;
+                    handler.sendMessage(msg);
+                    progress += 10;
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Message msg = handler.obtainMessage();//同 new Message();
+                msg.arg1 = -1;
+                handler.sendMessage(msg);
+            }
+        };
+
+
+        Button button = (Button) findViewById(R.id.btnStart);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread workThread = new Thread(null, myWorker, "WorkThread");
+                workThread.start();
+
+            }
+        });
+    }
+}
